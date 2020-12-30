aeec2fa9a89cfe04ac6267a15dde51d656323770
Dmitriy Zhigulskiy <dimahts990@gmail.com>
2020-12-30 04:24:27 +0300

    Работоспособная пушка (пока просто поварачивается)

---
+++ Assets/script/frontend/gunMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunMove : MonoBehaviour
+{
+    private RaycastHit hit;
+    private bool readyFier;
+
+    public GameObject gun,gunPlatform;
+    private void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit) && hit.transform.CompareTag("web"))
+            {
+                readyFier = true;
+                gun.transform.LookAt(hit.point);
+                gunPlatform.transform.LookAt(new Vector3(hit.point.x,0.35f,hit.point.z));
+            }
+            else Fier();
+        }
+        if (Input.GetMouseButtonUp(0)) Fier();
+
+    }
+    private void Fier()
+    {
+        if (readyFier == true)
+        {
+            Debug.Log("fier");
+            readyFier = false;
+        }
+    }
+}
