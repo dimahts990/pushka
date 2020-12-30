cebcef4969e8affedd5887ee0b33e7d9589777ef
Dmitriy Zhigulskiy <dimahts990@gmail.com>
2020-12-30 10:54:12 +0300

    красивое вращение)

--- Assets/script/backend/barrelGen.cs
+++ Assets/script/backend/barrelGen.cs
@@ -6,37 +6,40 @@ public class barrelGen : MonoBehaviour
 {
     private float t;
     public float timer;
-    public int line;
     public GameObject barrel;
     void Update()
     {
         if (t <= 0)
         {
-            gen();
+            gen(Random.Range(1,5));
             t = timer;
         }
         else t-=Time.deltaTime;        
     }
 
-    void gen()
+    void gen(int line)
     {
         GameObject newBarrel;
         switch (line)
         {
             case 1:
-                newBarrel = Instantiate(barrel, new Vector3(Random.Range(-2.4f, 2.4f), -3.1f, 10), Quaternion.identity);
+                newBarrel = Instantiate(barrel, new Vector3(Random.Range(-2.4f, 2.4f), -3.1f, 10), Quaternion.Euler(new Vector3(Random.Range(45f,136f),90f,0f)));
+                newBarrel.GetComponent<barrelSetting>().type = Random.Range(1, 3);
                 newBarrel.GetComponent<Rigidbody>().AddForce(transform.up * Random.Range(480f,670f));
                 break;
             case 2:
-                newBarrel = Instantiate(barrel, new Vector3(Random.Range(-3f, 3.2f), -4.1f, 13), Quaternion.identity);
+                newBarrel = Instantiate(barrel, new Vector3(Random.Range(-3f, 3.2f), -4.1f, 13), Quaternion.Euler(new Vector3(Random.Range(45f, 136f), 90f, 0f)));
+                newBarrel.GetComponent<barrelSetting>().type = Random.Range(1, 3);
                 newBarrel.GetComponent<Rigidbody>().AddForce(transform.up * Random.Range(520f, 770f));
                 break;
             case 3:
-                newBarrel = Instantiate(barrel, new Vector3(Random.Range(-4f, 4f), -5f, 16), Quaternion.identity);
+                newBarrel = Instantiate(barrel, new Vector3(Random.Range(-4f, 4f), -5f, 16), Quaternion.Euler(new Vector3(Random.Range(45f, 136f), 90f, 0f)));
+                newBarrel.GetComponent<barrelSetting>().type = Random.Range(1, 3);
                 newBarrel.GetComponent<Rigidbody>().AddForce(transform.up * Random.Range(570f, 860f));
                 break;
             case 4:
-                newBarrel = Instantiate(barrel, new Vector3(Random.Range(-5f, 5f), -6f, 19), Quaternion.identity);
+                newBarrel = Instantiate(barrel, new Vector3(Random.Range(-5f, 5f), -6f, 19), Quaternion.Euler(new Vector3(Random.Range(45f, 136f), 90f, 0f)));
+                newBarrel.GetComponent<barrelSetting>().type = Random.Range(1, 3);
                 newBarrel.GetComponent<Rigidbody>().AddForce(transform.up * Random.Range(680f, 930f));
                 break;
         }
---
+++ Assets/script/backend/barrelSetting.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barrelSetting : MonoBehaviour
+{
+    private GameObject barrel;
+    public int type;
+    void Start()
+    {
+        barrel = this.gameObject;
+        switch (type)
+        {
+            case 1:
+                Debug.Log($"обычная бочка дающая {Random.Range(1, 6).ToString()} снарядов");
+                break;
+            case 2:
+                Debug.Log("бочка, которая должна всё взорвать");
+                break;
+        }
+    }
+
+    void Update()
+    {
+        if (barrel.transform.position.y <= -6.5f) Destroy(barrel);
+        transform.Rotate(new Vector3(1, 1, 1));
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "core")
+        {
+            Die(other.gameObject);
+        }
+    }
+
+    void Die(GameObject obj)
+    {
+        Destroy(obj);
+        Destroy(this.gameObject);
+    }
+}
